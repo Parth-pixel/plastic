132cd786486967ddc3e71d0f54f84a726e139f8c
Parth-pixel <74820219+Parth-pixel@users.noreply.github.com>
2020-12-31 20:08:55 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,8 @@
 var personIMG, personSprite, plasticSprite,plastic1Sprite,plastic2Sprite,plastic1IMG,plastic2IMG;
 var ocean, oceanIMG,backgroundimage;
 var score;
+var plasticSpritegroup;
+var gamestate = "play";
 //var packageBody,ground;
 //var red1, red2, red3;
 const Engine = Matter.Engine;
@@ -10,59 +12,37 @@ const Body = Matter.Body;
 
 function preload()
 {
-	personIMG=loadImage("swimming.png");
-	plasticIMG=loadImage("bag.jpg");
-	plastic1IMG=loadImage("plasticbottlecrushed.jpg");
-	plastic2IMG=loadImage("bottle.png");
+	personIMG=loadImage("energetic swimmer.png");
+	plasticIMG=loadImage("bottle.png");
+	plastic1IMG=loadImage("plasticbottlecrushed.png");
+	plastic2IMG=loadImage("bottle2.png");
 	oceanIMG=loadImage("ocean.jpg");
 	backgroundimage = loadImage("sand.jpg")
 }
 
 function setup() {
-	createCanvas(450, 700);
+	createCanvas(1300, 400);
 	//rectMode(CENTER);
 	
 	score = 0;
-	ocean=createSprite(width/2, height-35, width,10);
+	ocean=createSprite(650,300,1300,50);//width/2, height-35, width,10);
 	//groundSprite.shapeColor=color(255)
 	ocean.addImage(oceanIMG);
-	ocean.scale = 2.5;
+	ocean.scale = 2;
 
 
-	
-
-	
-	plasticSprite=createSprite(100, 580, 10,10);
-	//plasticSprite.addImage(plasticIMG)
-	plasticSprite.scale=0.3
-
+	plasticSpritegroup = createGroup();
 
 	
-	plastic1Sprite=createSprite(100,580, 10,10);
-	//plastic1Sprite.addImage(plastic1IMG)
-	plastic1Sprite.scale=0.4
+	
 
-	plastic2Sprite=createSprite(100, 580, 10,10);
-	//plastic2Sprite.addImage(plastic2IMG)
-	plastic2Sprite.scale=0.5
-
-	personSprite=createSprite(50, 580, 10,10);
+	personSprite=createSprite(100, 350, 10,10);
 	personSprite.addImage(personIMG)
-	personSprite.scale=0.3
+	personSprite.scale=0.2;
 	
 	
 	
-	//red1 = createSprite(400,650,200,20);
-	//red1.shapeColor = color(139,0,0);
 	
-
-	//red2 = createSprite(300,600,20,120);
-	//red2.shapeColor = color(139,0,0);
-
-	
-
-	//red3 = createSprite(500,600,20,120);
-	//red3.shapeColor = color(139,0,0);
 	
 
 	engine = Engine.create();
@@ -83,49 +63,86 @@ function setup() {
 
 
 function draw() {
-	if (keyDown(RIGHT_ARROW)){
-		personSprite.x = personSprite.x + 2;
+	personSprite.debug = true;
+	personSprite.setCollider("rectangle",0,0,700,500)
+	if (gamestate === "play"){
+	background("white");//backgroundimage);
+
+	if (ocean.x < 0){
+		ocean.x =ocean.width/2;
+	  }
+	
+	
+
+	if (keyDown(UP_ARROW)){
+		personSprite.y = personSprite.y - 2;
 	}
-	text("Score: "+ score, 500,50);
-	personSprite.depth = personSprite.depth+1;
-	keyPressed();
+	if (keyDown(DOWN_ARROW)){
+		personSprite.y = personSprite.y+2;
+	}
+	//personSprite.depth = personSprite.depth+1;
+	
 	plastic();
   rectMode(CENTER);
-  background(backgroundimage);
-  ocean.velocityX = -(4 + 3* score/100)
   
-  if (ocean.x < 0){
-	ocean.x = ocean.width/2;
-  }
-  score = score + Math.round(getFrameRate()/60);
+  ocean.velocityX = -(3 + 5* score/10)
+  
+  
+ 
  // packageSprite.x= packageBody.position.x 
   //packageSprite.y= packageBody.position.y 
+  
   drawSprites();
- 
+  text("Score: "+ score, 500,50);
+  if (personSprite.isTouching(plasticSpritegroup)){
+	plasticSpritegroup.destroyEach();
+	score = score+1;
+	
+}
+}
+if (gamestate === "End"){
+	    ocean.velocityX = 0;
+		plasticSpritegroup.velocityX = 0;
+		plasticSpritegroup.destroyEach();
+		background(0);
+		fill("white");
+		textSize(100);
+		text("Congratulations You Have successfully cleaned the Ocean", 200,200);
+}
+if (score === 20){
+	gamestate = "End";
+}
 }
 
-function keyPressed() {
- if (keyCode === LEFT_ARROW) {
 
-	Matter.Body.setStatic(packageBody, false);
-  }
-}
 
 function plastic(){
-	if (frameCount % 10 === 0){
-		
+	if (frameCount % 100 === 0){
+		plasticSprite=createSprite(700, 380, 10,10);
+	//plasticSprite.addImage(plasticIMG)
+	plasticSprite.scale=0.1
+	plasticSpritegroup.add(plasticSprite);
+    plasticSprite.y = Math.round(random(100,500));
+	
+	//plastic1Sprite=createSprite(700,380, 10,10);
+	//plastic1Sprite.addImage(plastic1IMG)
+	//plastic1Sprite.scale=0.4
+
+//	plastic2Sprite=createSprite(1300, 350, 10,10);
+	//plastic2Sprite.addImage(plastic2IMG)
+	//plastic2Sprite.scale=0.1;
 		plasticSprite.velocityX = -6
-		plastic1Sprite.velocityX = -6
-		plastic2Sprite.velocityX = -6
+		//plastic1Sprite.velocityX = -6
+		//plastic2Sprite.velocityX = -6
 		
 		 //generate random obstacles
 		 var rand = Math.round(random(1,3));
 		 switch(rand) {
 		   case 1: plasticSprite.addImage(plasticIMG);
 				   break;
-		   case 2: plastic1Sprite.addImage(plastic1IMG);
+		   case 2: plasticSprite.addImage(plastic1IMG);
 				   break;
-		   case 3: plastic2Sprite.addImage(plastic2IMG);
+		   case 3: plasticSprite.addImage(plastic2IMG);
 				   break;
 
 
